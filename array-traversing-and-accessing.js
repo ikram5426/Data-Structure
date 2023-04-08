8f356bb04911f49b1411f10479e6ba4b5794bf84
IkramKhan <ikram5426@gmail.com>
2023-04-08 13:16:04 +0500

    Array traversing and accessing

---
+++ array-traversing-and-accessing.js
@@ -0,0 +1,15 @@
+let arr=[3,5,8,2,1,9,0,56,43,22]
+
+// traverse through array elements
+
+// I=0  is the element from where operation array will start
+//i<arr.length is the number of operation perform on array elements after that operation will stop 
+// i++ is the logic we want to perform on each element of array
+for (let i=0; i<arr.length; i++){
+  // console.log(arr[i]);
+}
+
+// Accessing array elements
+
+let index=5
+console.log(arr[index])
